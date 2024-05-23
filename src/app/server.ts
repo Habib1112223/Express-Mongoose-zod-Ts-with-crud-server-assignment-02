79c4d336318267576d45c345611d24972f5b9472
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-05-23 17:07:10 +0600

    server.ts create the project

---
+++ src/app/server.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import app from './app';
+import 'dotenv/config'
+
+(
+    async function () {
+
+        try {
+
+            await mongoose.connect(process.env.MONGODB_URL as string);
+            console.log('Pinged your deployment. You successfully connected to MongoDB!');
+
+            app.listen(5000, () => {
+                console.log(`Assignment-2 listening on port ${5000}`);
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+)();
